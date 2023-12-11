3ccd6e1d2efb984e917927185dc17b3e020f0a78
aizzatjali86 <aizzat.sazali@gmail.com>
2023-12-11 23:27:50 +0800

    Updates

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-from ultralytics import YOLO
 import cv2
 
 
-# Load a model
-model = YOLO('yolov8m-pose.pt')  # load an official model
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
 
-# Predict with the model
-results = model(r'D:\PycharmPojects\Pose_Estimation_to_Animation\bus.jpg')
-#image = cv2.imread(r'D:\PycharmPojects\Pose_Estimation_to_Animation\bus.jpg')
 
-image = results[0].plot()
-print(results[0])
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
 
-cv2.imshow("", image)
-cv2.waitKey(0)
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
+
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# STEP 2: Create an PoseLandmarker object.
+base_options = python.BaseOptions(model_asset_path='D:\PycharmPojects\Pose_Estimation_to_Animation\pose_landmarker_heavy.task')
+options = vision.PoseLandmarkerOptions(
+    base_options=base_options,
+    output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
+
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture(r'x2mate.com-THROWING Knives (online-video-cutter.com).mp4')
+print(cap.isOpened())
+while cap.isOpened():
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # STEP 3: Load the input image.
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+    # STEP 4: Detect pose landmarks from the input image.
+    detection_result = detector.detect(image)
+
+    # STEP 5: Process the detection result. In this case, visualize it.
+    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+    cv2.imshow("t", annotated_image)
+    #cv2.waitKey(0)
+    if cv.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
